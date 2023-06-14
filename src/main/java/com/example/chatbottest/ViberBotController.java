c9a6c945d01716dd683cd1df615c4f334f4def87
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-06-14 17:28:34 +0900

    로드벨런싱 health check를 위해 코드 수정

--- src/main/java/com/example/chatbottest/ViberBotController.java
+++ src/main/java/com/example/chatbottest/ViberBotController.java
@@ -1,13 +1,17 @@
 package com.example.chatbottest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ViberBotController {
 
-    @PostMapping("/webhook")
+    @GetMapping(value = "/api/health")
+    public String healthCheck() {return "Health check completed.";}
+
+    @PostMapping(value = "/api/bot/test")
     public String webhookTest() {
-        return "hello world";
+        return "hello bot";
     }
 }
