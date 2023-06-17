aadb25a9e7f40aa9b7de6d00fad749644c79a2ee
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-06-17 23:45:47 +0900

    build docker image

--- src/main/java/com/example/chatbottest/ViberBotController.java
+++ src/main/java/com/example/chatbottest/ViberBotController.java
@@ -1,28 +1,11 @@
 package com.example.chatbottest;
 
-import com.google.common.base.Charsets;
-import com.google.common.base.Preconditions;
-import com.google.common.io.CharStreams;
-import com.viber.bot.Request;
-import com.viber.bot.ViberSignatureValidator;
-import com.viber.bot.api.ViberBot;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Nullable;
-import javax.inject.Inject;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.concurrent.ExecutionException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ViberBotController {
-
-    @Inject
-    private ViberBot bot;
-
-    @Inject
-    private ViberSignatureValidator signatureValidator;
 
     @GetMapping(value = "/api/health")
     public String healthCheck() {return "Health check completed.";}
@@ -32,12 +15,4 @@ public class ViberBotController {
         return "hello bot";
     }
 
-    @PostMapping(value = "/incomming", produces = "application/json")
-    public String incoming(@RequestBody String json,
-                           @RequestHeader("X-Viber-Content-Signature") String serverSideSignature)
-            throws ExecutionException, InterruptedException, IOException {
-        Preconditions.checkState(signatureValidator.isSignatureValid(serverSideSignature, json), "invalid signature");
-        @Nullable InputStream response = bot.incoming(Request.fromJsonString(json)).get();
-        return response != null ? CharStreams.toString(new InputStreamReader(response, Charsets.UTF_8)) : null;
-    }
 }
