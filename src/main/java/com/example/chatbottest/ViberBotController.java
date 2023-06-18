4e1595f32c881e412481cbfe332ec009374c8ddd
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-06-18 15:45:11 +0900

    hello world

--- src/main/java/com/example/chatbottest/ViberBotController.java
+++ src/main/java/com/example/chatbottest/ViberBotController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ViberBotController {
 
+    @GetMapping(value = "/")
+    public String helloWorld() {return "Hello World";}
+
     @GetMapping(value = "/api/health")
     public String healthCheck() {return "Health check completed.";}
 
