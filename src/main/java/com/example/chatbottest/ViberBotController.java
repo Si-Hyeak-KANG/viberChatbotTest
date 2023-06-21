a6c2fd88edf2090a791af81d636bebddeb2f7d3c
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-06-22 08:43:48 +0900

    send Message test

--- src/main/java/com/example/chatbottest/ViberBotController.java
+++ src/main/java/com/example/chatbottest/ViberBotController.java
@@ -1,5 +1,6 @@
 package com.example.chatbottest;
 
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,16 +8,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 
 @RestController
 public class ViberBotController {
 
     static String event = "";
+    static boolean isMessage = false;
 
     @GetMapping(value = "/")
     public String helloWorld() {
-        return event;
+        String result = event;
+        if (isMessage) {
+           result += " send Message";
+        }
+        return result;
     }
 
     @GetMapping(value = "/api/health")
@@ -28,7 +36,9 @@ public class ViberBotController {
     public ResponseEntity<WelcomeMessage> webhookTest(@RequestBody HashMap<String, Object> request) throws IOException {
 
         event = request.get("event").toString();
+        if (event.equals("message")) sendMessageToUser("메시지 받으셨나요?", "https://chatapi.viber.com/pa/send_message");
 
+        // 이미지 URL 중요함.....
         return ResponseEntity.ok(WelcomeMessage.builder()
                 .sender(new Sender("John McClane","https://picsum.photos/id/237/200/300"))
                 .trackingData("tracking data")
@@ -48,19 +58,21 @@ public class ViberBotController {
 //    }
 //
 //
-//    private static void sendMessageToUser(String text, String url) throws IOException {
-//        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
-//        con.setRequestMethod("POST");
-//        con.setRequestProperty("Content-Type", "application/json");
-//        con.setRequestProperty("X-Viber-Auth-Token", "51325d70dda7dc2c-2f10319fe1a387be-b00bd73e2ba977c2");
-//        JSONObject data = new JSONObject();
-//        data.put("receiver", "zEjO1IAIu/GE1o2CDJ3g/Q==");
-//        data.put("min_api_version", 1);
-//        data.put("sender", new Sender("test", "https://avatars.githubusercontent.com/u/79829085?v=4"));
-//        data.put("tracking_data", "tracking data");
-//        data.put("type","text");
-//        data.put("text", text);
-//        con.setDoOutput(true);
-//        con.getOutputStream().write(data.toString().getBytes());
-//    }
+    private static void sendMessageToUser(String text, String url) throws IOException {
+        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("X-Viber-Auth-Token", "51325d70dda7dc2c-2f10319fe1a387be-b00bd73e2ba977c2");
+        JSONObject data = new JSONObject();
+        data.put("receiver", "YZmWEo/KBmn/6PKQW0qBxg==");
+        data.put("min_api_version", 1);
+        data.put("sender", new Sender("test", "https://picsum.photos/id/237/200/300"));
+        data.put("tracking_data", "tracking data");
+        data.put("type","text");
+        data.put("text", text);
+        con.setDoOutput(true);
+        con.getOutputStream().write(data.toString().getBytes());
+
+        isMessage = true;
+    }
 }
