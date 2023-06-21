d55d084b44d346a4692209ffd1808b80d1827d8c
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-06-21 19:30:44 +0900

    deploy welcome

--- src/main/java/com/example/chatbottest/WelcomeMessage.java
+++ src/main/java/com/example/chatbottest/WelcomeMessage.java
@@ -1,5 +1,6 @@
 package com.example.chatbottest;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,7 +9,10 @@ import lombok.Data;
 public class WelcomeMessage {
 
     Sender sender;
+
+    @JsonProperty("tracking_data")
     String trackingData;
+
     String type;
     String text;
     String media;
