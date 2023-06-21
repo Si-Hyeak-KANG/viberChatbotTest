e86fefa6b3f5b138f715cd9b66252cf71994c1ce
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-06-21 17:58:26 +0900

    welcome message test

--- src/main/java/com/example/chatbottest/ViberBotController.java
+++ src/main/java/com/example/chatbottest/ViberBotController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 @RestController
@@ -30,50 +29,52 @@ public class ViberBotController {
 
     @PostMapping(value = "/api/bot/test")
     public ResponseEntity<? extends CallBackMessage> webhookTest(@RequestBody String json) throws IOException {
-        String event = new JSONObject(json).getString("event");
-        currEvent = event;
-        return switch (event) {
-            case "conversation_started" -> sendWelcomeMessage(json);
-            case "message" -> userRequest(json);
-            default -> throw new IllegalStateException("Unexpected value: " + event);
-        };
-    }
 
-    private ResponseEntity<SuccessMessage> userRequest(String json) throws IOException {
-        String text = new JSONObject(json).getJSONObject("message").getString("text");
-        currUserInputText = text;
-        String url = "https://chatapi.viber.com/pa/send_message";
-        sendMessageToUser(text, url);
-        return ResponseEntity.ok(new SuccessMessage());
-    }
-
-    public ResponseEntity<WelcomeMessage> sendWelcomeMessage(String json) {
         return ResponseEntity.ok(
                 new WelcomeMessage(
-                new Sender("test", "https://avatars.githubusercontent.com/u/79829085?v=4"),
-                "tracking data",
+                        new Sender("test", "https://avatars.githubusercontent.com/u/79829085?v=4"),
+                        "tracking data",
                         "picture",
                         "Welcome",
                         "https://avatars.githubusercontent.com/u/79829085?v=4",
                         "https://avatars.githubusercontent.com/u/79829085?v=4")
         );
-
     }
 
-    private static void sendMessageToUser(String text, String url) throws IOException {
-        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("X-Viber-Auth-Token", "51325d70dda7dc2c-2f10319fe1a387be-b00bd73e2ba977c2");
-        JSONObject data = new JSONObject();
-        data.put("receiver", "zEjO1IAIu/GE1o2CDJ3g/Q==");
-        data.put("min_api_version", 1);
-        data.put("sender", new Sender("test", "https://avatars.githubusercontent.com/u/79829085?v=4"));
-        data.put("tracking_data", "tracking data");
-        data.put("type","text");
-        data.put("text", text);
-        con.setDoOutput(true);
-        con.getOutputStream().write(data.toString().getBytes());
-    }
+//    private ResponseEntity<SuccessMessage> userRequest(String json) throws IOException {
+//        String text = new JSONObject(json).getJSONObject("message").getString("text");
+//        currUserInputText = text;
+//        String url = "https://chatapi.viber.com/pa/send_message";
+//        sendMessageToUser(text, url);
+//        return ResponseEntity.ok(new SuccessMessage());
+//    }
+//
+//    public ResponseEntity<WelcomeMessage> sendWelcomeMessage(String json) {
+//        return ResponseEntity.ok(
+//                new WelcomeMessage(
+//                        new Sender("test", "https://avatars.githubusercontent.com/u/79829085?v=4"),
+//                        "tracking data",
+//                        "picture",
+//                        "Welcome",
+//                        "https://avatars.githubusercontent.com/u/79829085?v=4",
+//                        "https://avatars.githubusercontent.com/u/79829085?v=4")
+//        );
+//    }
+//
+//    private static void sendMessageToUser(String text, String url) throws IOException {
+//        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+//        con.setRequestMethod("POST");
+//        con.setRequestProperty("Content-Type", "application/json");
+//        con.setRequestProperty("X-Viber-Auth-Token", "51325d70dda7dc2c-2f10319fe1a387be-b00bd73e2ba977c2");
+//        JSONObject data = new JSONObject();
+//        data.put("receiver", "zEjO1IAIu/GE1o2CDJ3g/Q==");
+//        data.put("min_api_version", 1);
+//        data.put("sender", new Sender("test", "https://avatars.githubusercontent.com/u/79829085?v=4"));
+//        data.put("tracking_data", "tracking data");
+//        data.put("type","text");
+//        data.put("text", text);
+//        con.setDoOutput(true);
+//        con.getOutputStream().write(data.toString().getBytes());
+//    }
 
 }
