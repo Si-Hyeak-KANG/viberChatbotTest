155b0d9fb1a5981748ef82d6006e174805c8d367
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-06-22 11:59:20 +0900

    re

--- src/main/java/com/example/chatbottest/ViberBotController.java
+++ src/main/java/com/example/chatbottest/ViberBotController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Slf4j
 @RestController
@@ -38,40 +39,40 @@ public class ViberBotController {
 
         log.info("event = {}, tracking_data = {}", event, trackingData);
 
-        if (event == "message") {
-            log.info("메시지 전송 시작");
-            String senderId = new JSONObject(callback).getJSONObject("sender").getString("id");
-            String senderName = new JSONObject(callback).getJSONObject("sender").getString("name");
-            String senderAvatar = new JSONObject(callback).getJSONObject("sender").getString("avatar");
-
-            log.info("id = {}, name = {}, avatar = {}", senderId, senderName, senderAvatar);
-
-            Sender receiver = new Sender(senderName, senderAvatar);
-            String url = "https://chatapi.viber.com/pa/send_message";
-
-            SendMessage body = SendMessage.builder()
-                    .receiver(senderId)
-                    .minApiVersion(1)
-                    .sender(receiver)
-                    .trackingData(trackingData)
-                    .type("text")
-                    .text("멍멍")
-                    .build();
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.add("X-Viber-Auth-Token","51375b70e3a7e340-3874f3e396e15f3c-cd7134f6cda50c20");
-            HttpEntity<SendMessage> request = new HttpEntity<>(body, headers);
-
-            RestTemplate rt = new RestTemplate();
-            ResponseEntity<String> response = rt.postForEntity(url, request, String.class);
-
-            if (response.getStatusCode() == HttpStatus.OK) {
-                System.out.println("Message sent successfully.");
-            } else {
-                System.out.println("Failed to send message.");
-            }
-        }
+//        if (Objects.equals(event, "message")) {
+//            log.info("메시지 전송 시작");
+//            String senderId = new JSONObject(callback).getJSONObject("sender").getString("id");
+//            String senderName = new JSONObject(callback).getJSONObject("sender").getString("name");
+//            String senderAvatar = new JSONObject(callback).getJSONObject("sender").getString("avatar");
+//
+//            log.info("id = {}, name = {}, avatar = {}", senderId, senderName, senderAvatar);
+//
+//            Sender receiver = new Sender(senderName, senderAvatar);
+//            String url = "https://chatapi.viber.com/pa/send_message";
+//
+//            SendMessage body = SendMessage.builder()
+//                    .receiver(senderId)
+//                    .minApiVersion(1)
+//                    .sender(receiver)
+//                    .trackingData(trackingData)
+//                    .type("text")
+//                    .text("멍멍")
+//                    .build();
+//
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.setContentType(MediaType.APPLICATION_JSON);
+//            headers.add("X-Viber-Auth-Token","51375b70e3a7e340-3874f3e396e15f3c-cd7134f6cda50c20");
+//            HttpEntity<SendMessage> request = new HttpEntity<>(body, headers);
+//
+//            RestTemplate rt = new RestTemplate();
+//            ResponseEntity<String> response = rt.postForEntity(url, request, String.class);
+//
+//            if (response.getStatusCode() == HttpStatus.OK) {
+//                System.out.println("Message sent successfully.");
+//            } else {
+//                System.out.println("Failed to send message.");
+//            }
+//        }
 
         // 이미지 URL 중요함.....
         return ResponseEntity.ok(WelcomeMessage.builder()
