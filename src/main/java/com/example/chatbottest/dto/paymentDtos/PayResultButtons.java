5e687d8042723e0ec66e35a7790929fad56e0d4b
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-07-02 20:09:56 +0900

    format test

--- src/main/java/com/example/chatbottest/dto/paymentDtos/PayResultButtons.java
+++ src/main/java/com/example/chatbottest/dto/paymentDtos/PayResultButtons.java
@@ -26,15 +26,14 @@ public class PayResultButtons {
      * 12) 버튼 (terms of use)
      */
     public static List<PayResultButtons> of() {
-        return List.of(ButtonsForImage.of(6, 1, "none", "https://www.google.com", "https://picsum.photos/id/237/200/300"),
+        return List.of(ButtonsForImage.of(6, 2, "none", "https://www.google.com", "https://picsum.photos/id/237/200/300"),
                 ButtonsForText.of(6, 1, "none", "https://www.google.com", "<font color=#323232><b> Cheeseburger Solo </b></font>", "large", "middle", "left"),
-ButtonsForText.of(6, 1, "none", "브랜드명", "<font color=#323232>" + "Mcdonald's" + "</font>", "small", "middle", "left"),
-ButtonsForText.of(6, 2, "none", "발신자명,메시지", "<font color=#323232> FROM." + "강시혁" + "</font><br><br>" + "<font color=#323232> message: &quot;" + "생일 축하한다~!" + "&quot; </font>", "medium", "middle", "left"),
-ButtonsForText.of(6, 2, "none", "코드,만료날짜", "<font color=#323232><b>Code : " + "dsfksjdfkjsdfhisdholvaksdfdsfadfvfdva" + "</b></font><br><font color=#323232><b>Expire Date : " + "02/07/23" + "</b></font>", "small", "middle", "left"),
-ButtonsForText.of(6, 3, "none", "수신자명,성공", "<font color=#323232><b>" + "제임스" + " 님의 이메일로 선물을 성공적으로 전송했습니다.</b></font>", "medium", "middle", "left"),
-ButtonsForText.of(6, 2, "none", "포인트내역", "<font color=#FF0115>사용 포인트 : " + "500p" + "</font><br><font color=#323232>남은 포인트 : " + "9500p" + "</font>", "medium", "middle", "left"),
-ButtonsForButton.of(6, 1, "open-url", "https://www.google.com", "#6863F2", "<font color=#FFFFFF>see participating stores</font>", "small", "middle", "middle"),
-ButtonsForButton.of(6, 1, "open-url", "https://www.google.com", "#6863F2", "<font color=#FFFFFF>terms of use</font>", "small", "middle", "middle"));
+                ButtonsForText.of(6, 1, "none", "브랜드명", "<font color=#323232>" + "Mcdonald's" + "</font>", "small", "top", "left"),
+                ButtonsForText.of(6, 3, "none", "발신자명,메시지", "<font color=#323232> From." + " 강시혁" + "</font>" + "<font color=#323232> Message: 생일 축하한다~!</font>"+"<font color=#323232><b>Code : " + "dsfksjdfkjsdfhisdholvaksdfds" + "</b></font><font color=#323232><b>Expire Date : " + "02/07/23" + "</b></font>", "medium", "middle", "left"),
+                ButtonsForText.of(6, 3, "none", "수신자명,성공", "<font color=#323232><b>" + "제임스" + " 님의 이메일로 선물을 성공적으로 전송했습니다.</b></font>", "medium", "middle", "left"),
+                ButtonsForText.of(6, 2, "none", "포인트내역", "<font color=#FF0115>사용 포인트 : " + "500p" + "</font><br><font color=#323232>남은 포인트 : " + "9500p" + "</font>", "medium", "middle", "left"),
+                ButtonsForButton.of(6, 1, "open-url", "https://www.google.com", "#6863F2", "<font color=#FFFFFF>see participating stores</font>", "small", "middle", "middle"),
+                ButtonsForButton.of(6, 1, "open-url", "https://www.google.com", "#6863F2", "<font color=#FFFFFF>terms of use</font>", "small", "middle", "middle"));
     }
 
     @Getter
@@ -95,7 +94,7 @@ ButtonsForButton.of(6, 1, "open-url", "https://www.google.com", "#6863F2", "<fon
 
         public static ButtonsForText of(int columns, int rows, String actionType, String actionBody, String text
                 , String textSize, String textVAlign, String textHAlign) {
-            return new ButtonsForText(columns, rows,actionType, actionBody,text,textSize, textVAlign, textHAlign);
+            return new ButtonsForText(columns, rows, actionType, actionBody, text, textSize, textVAlign, textHAlign);
         }
     }
 
@@ -133,7 +132,7 @@ ButtonsForButton.of(6, 1, "open-url", "https://www.google.com", "#6863F2", "<fon
 
         public static ButtonsForButton of(int columns, int rows, String actionType, String actionBody
                 , String bgColor, String text, String textSize, String textVAlign, String textHAlign) {
-            return new ButtonsForButton(columns, rows,actionType, actionBody, bgColor, text, textSize, textVAlign, textHAlign);
+            return new ButtonsForButton(columns, rows, actionType, actionBody, bgColor, text, textSize, textVAlign, textHAlign);
         }
     }
 }
